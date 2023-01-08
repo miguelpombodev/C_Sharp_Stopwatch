67de9c48e09aa7864f7467845d89e2ba89aaac99
Miguel Pombo <mp96dev@gmail.com>
2023-01-07 21:06:18 -0300

    feat: creating starting method

--- Program.cs
+++ Program.cs
@@ -1,7 +1,24 @@
-﻿namespace Stopwatch {
+﻿using System;
+using System.Threading;
+
+namespace Stopwatch {
     class Program {
         static void Main (string[] args) {
-            
+            Start(6);
+        }
+
+        static void Start(int time) {
+            int currentTime = 0;
+
+            while(currentTime != time) {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+
+            Console.Clear();
+            Console.WriteLine("Stopwatch finished... Returning to Menu");
         }
     }
 }
