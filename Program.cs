87b1f1c3c48acbe7af48a804be102e6980bb3a0c
Miguel Pombo <mp96dev@gmail.com>
2023-01-07 21:55:17 -0300

    feat: finishing MenuApp method and adding a sleep in the end of start method

--- Program.cs
+++ Program.cs
@@ -4,10 +4,12 @@ using System.Threading;
 namespace Stopwatch {
     class Program {
         static void Main (string[] args) {
-            Start(6);
+            MenuApp();
         }
 
         static void MenuApp() {
+            int multiplier = 1;
+
             Console.Clear();
             Console.WriteLine("S = Seconds => 10s = 10 seconds");
             Console.WriteLine("M = Minutes => 1m = 1 minute");
@@ -15,6 +17,18 @@ namespace Stopwatch {
             Console.WriteLine("How long will it count?");
 
             string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring((data.Length - 1),1));
+            int time = int.Parse(data.Substring(0, (data.Length - 1)));
+
+            if (type == 'm') {
+                multiplier = 60;
+            }
+
+            if (time == 0) {
+                System.Environment.Exit(0);
+            }
+
+            Start(time * multiplier);
         }
 
         static void Start(int time) {
@@ -29,6 +43,8 @@ namespace Stopwatch {
 
             Console.Clear();
             Console.WriteLine("Stopwatch finished... Returning to Menu");
+            Thread.Sleep(2000);
+            MenuApp();
         }
 
     }
